ec73934a6ae1cd9a9f5fff3a64c1fdf82cd01fa9
CesarCardozo <srfeudalwow@gmail.com>
2018-11-22 12:35:37 -0500

    Update Regulation.java

--- src/main/java/com/inside/models/entities/Regulation.java
+++ src/main/java/com/inside/models/entities/Regulation.java
@@ -6,31 +6,4 @@ public class Regulation {
 	private Rule rule;
 	private Event event;
 	
-	public Regulation(String idRegulations, Rule rule, Event event) {
-		super();
-		this.idRegulation = idRegulations;
-		this.rule = rule;
-		this.event = event;
-	}
-	
-	public String getIdRegulation() {
-		return idRegulation;
-	}
-	public void setIdRegulation(String idRegulations) {
-		this.idRegulation = idRegulations;
-	}
-	public Rule getRule() {
-		return rule;
-	}
-	public void setRule(Rule rule) {
-		this.rule = rule;
-	}
-	public Event getEvent() {
-		return event;
-	}
-	public void setEvent(Event event) {
-		this.event = event;
-	}
-	
-	
 }
