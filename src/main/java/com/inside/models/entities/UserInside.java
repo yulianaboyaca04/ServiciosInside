e24e8b941ddb44df6b1c1b7d44755c717775d7cb
NilsonGil <38057682+NilsonGil@users.noreply.github.com>
2018-11-22 12:27:42 -0500

    Update UserInside.java

--- src/main/java/com/inside/models/entities/UserInside.java
+++ src/main/java/com/inside/models/entities/UserInside.java
@@ -1,5 +1,17 @@
 package com.inside.models.entities;
 
-public class UserInside {
+import java.util.ArrayList;
+import java.util.Date;
 
+public class UserInside {
+	
+	private String idUser;
+	private Credentials credential;
+	private Image image;
+	private String nameUser;
+	private String lastName;
+	private Date birthDate;
+	private String nikename;
+	private ArrayList<Interest> userInteres;
+	
 }
