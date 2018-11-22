54ef11ad80ec2f8e8d564c540aa67f1942bce34e
yulianaboyaca04 <mary.boyaca@uptc.edu.co>
2018-11-22 12:33:08 -0500

    s

--- src/main/java/com/inside/models/entities/EventDate.java
+++ src/main/java/com/inside/models/entities/EventDate.java
@@ -1,5 +1,36 @@
 package com.inside.models.entities;
 
+import java.util.Date;
+
 public class EventDate {
 
+	private String idDate;
+	private Date dateStart;
+	private Date dateFinish;
+	public EventDate(String idDate, Date dateStart, Date dateFinish) {
+		super();
+		this.idDate = idDate;
+		this.dateStart = dateStart;
+		this.dateFinish = dateFinish;
+	}
+	public String getIdDate() {
+		return idDate;
+	}
+	public void setIdDate(String idDate) {
+		this.idDate = idDate;
+	}
+	public Date getDateStart() {
+		return dateStart;
+	}
+	public void setDateStart(Date dateStart) {
+		this.dateStart = dateStart;
+	}
+	public Date getDateFinish() {
+		return dateFinish;
+	}
+	public void setDateFinish(Date dateFinish) {
+		this.dateFinish = dateFinish;
+	}
+	
+	
 }
--- src/main/java/com/inside/models/entities/Regulation.java
+++ src/main/java/com/inside/models/entities/Regulation.java
@@ -2,4 +2,35 @@ package com.inside.models.entities;
 
 public class Regulation {
 
+	private String idRegulation;
+	private Rule rule;
+	private Event event;
+	
+	public Regulation(String idRegulations, Rule rule, Event event) {
+		super();
+		this.idRegulation = idRegulations;
+		this.rule = rule;
+		this.event = event;
+	}
+	
+	public String getIdRegulation() {
+		return idRegulation;
+	}
+	public void setIdRegulation(String idRegulations) {
+		this.idRegulation = idRegulations;
+	}
+	public Rule getRule() {
+		return rule;
+	}
+	public void setRule(Rule rule) {
+		this.rule = rule;
+	}
+	public Event getEvent() {
+		return event;
+	}
+	public void setEvent(Event event) {
+		this.event = event;
+	}
+	
+	
 }
