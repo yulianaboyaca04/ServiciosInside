e436f25829079d8b1efdcec8fbc9f33ad59cfbe4
yulianaboyaca04 <mary.boyaca@uptc.edu.co>
2018-11-22 12:35:32 -0500

    address

--- src/main/java/com/inside/models/entities/Address.java
+++ src/main/java/com/inside/models/entities/Address.java
@@ -2,4 +2,9 @@ package com.inside.models.entities;
 
 public class Address {
 
+	private String idAddress;
+	private float latitude;
+	private float longitude;
+	private String nameCity;
+	private String namePlace;
 }
