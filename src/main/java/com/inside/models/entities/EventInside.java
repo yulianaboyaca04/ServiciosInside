a241955a042b55b2161525d9b4ab448d195106ff
CesarCardozo <srfeudalwow@gmail.com>
2018-11-22 12:31:10 -0500

    Update EventInside.java

--- src/main/java/com/inside/models/entities/EventInside.java
+++ src/main/java/com/inside/models/entities/EventInside.java
@@ -1,5 +1,17 @@
 package com.inside.models.entities;
 
+import java.util.ArrayList;
+
 public class EventInside {
 
+	private String idEvent;
+	private UserInside userCreator;
+	private HowToBuy howToBuy;
+	private Address address;
+	private EventDate eventDate;
+	private String nameEvent;
+	private String descriptionEvent;
+	private Gallery gallery;
+	private ArrayList<Interest> eventInterests;
+	
 }
