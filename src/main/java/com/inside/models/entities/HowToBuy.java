8dede922a14c3072b7299c503aacfe2a92b6c490
yulianaboyaca04 <mary.boyaca@uptc.edu.co>
2018-11-22 12:23:52 -0500

    howTobuy

--- src/main/java/com/inside/models/entities/HowToBuy.java
+++ src/main/java/com/inside/models/entities/HowToBuy.java
@@ -1,5 +1,50 @@
 package com.inside.models.entities;
 
 public class HowToBuy {
+	
+	private String idHowToBuy;
+	private String descriptionHowToBuy;
+	private boolean inPresence;
+	private float price;
+
+	public HowToBuy(String idHowToBuy, String descriptionHowToBuy, boolean inPresence, float price) {
+		super();
+		this.idHowToBuy = idHowToBuy;
+		this.descriptionHowToBuy = descriptionHowToBuy;
+		this.inPresence = inPresence;
+		this.price = price;
+	}
+
+	public String getIdHowToBuy() {
+		return idHowToBuy;
+	}
+
+	public void setIdHowToBuy(String idHowToBuy) {
+		this.idHowToBuy = idHowToBuy;
+	}
+
+	public String getDescriptionHowToBuy() {
+		return descriptionHowToBuy;
+	}
+
+	public void setDescriptionHowToBuy(String descriptionHowToBuy) {
+		this.descriptionHowToBuy = descriptionHowToBuy;
+	}
+
+	public boolean isInPresence() {
+		return inPresence;
+	}
+
+	public void setInPresence(boolean inPresence) {
+		this.inPresence = inPresence;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
 
 }
