9430246d31c2f99415fa7bdc38719d80159aa624
yulianaboyaca04 <mary.boyaca@uptc.edu.co>
2018-11-23 12:59:31 -0500

    address

--- src/main/java/com/inside/models/entities/Address.java
+++ src/main/java/com/inside/models/entities/Address.java
@@ -1,5 +1,10 @@
 package com.inside.models.entities;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.inside.persistence.DataBaseAcces;
+
 public class Address {
 
 	private String idAddress;
@@ -7,4 +12,72 @@ public class Address {
 	private float longitude;
 	private String nameCity;
 	private String namePlace;
+	
+	public Address() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Address(String idAddress, float latitude, float longitude, String nameCity, String namePlace) {
+		this.idAddress = idAddress;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.nameCity = nameCity;
+		this.namePlace = namePlace;
+	}
+
+	public String getIdAddress() {
+		return idAddress;
+	}
+
+	public void setIdAddress(String idAddress) {
+		this.idAddress = idAddress;
+	}
+
+	public float getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(float latitude) {
+		this.latitude = latitude;
+	}
+
+	public float getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(float longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getNameCity() {
+		return nameCity;
+	}
+
+	public void setNameCity(String nameCity) {
+		this.nameCity = nameCity;
+	}
+
+	public String getNamePlace() {
+		return namePlace;
+	}
+
+	public void setNamePlace(String namePlace) {
+		this.namePlace = namePlace;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [idAddress=" + idAddress + ", latitude=" + latitude + ", longitude=" + longitude + ", nameCity="
+				+ nameCity + ", namePlace=" + namePlace + "]";
+	}
+	
+	public void insertIntoDataBase() throws SQLException {
+		PreparedStatement preparedStatement = DataBaseAcces.getInstance().getConnection().prepareStatement("INSERT INTO ADDRESS VALUES(?,?,?,?,?)");
+		preparedStatement.setString(1, this.idAddress);
+		preparedStatement.setFloat(2, this.latitude);
+		preparedStatement.setFloat(3, this.longitude);
+		preparedStatement.setString(4, this.nameCity);
+		preparedStatement.setString(5, this.namePlace);
+		preparedStatement.execute();
+	}
 }
