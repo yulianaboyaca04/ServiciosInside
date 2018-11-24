39936d7b8b3da88fe48314453f2d85600eed778a
CesarCardozo <srfeudalwow@gmail.com>
2018-11-24 12:05:44 -0500

    asd

---
+++ src/main/java/com/inside/persistence/PruebaPersistenciaCesar.java
@@ -0,0 +1,10 @@
+package com.inside.persistence;
+
+import com.inside.models.entities.Event;
+import com.inside.models.entities.EventInside;
+
+public class PruebaPersistenciaCesar {
+	
+	public static void main(String[] args) {
+	}
+}
